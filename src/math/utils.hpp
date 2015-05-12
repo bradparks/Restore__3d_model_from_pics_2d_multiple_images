fba0d4833aa76a06394a09216689c707df700d87
Kai Wolf <kai.wolf@gmail.com>
2015-05-12 20:07:49 +0200

    Add math utility functions

---
+++ src/math/utils.hpp
@@ -0,0 +1,78 @@
+/*
+ * Copyright (c) 2015, Kai Wolf
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE. */
+
+#ifndef RESTORE_MATH_UTILS_HPP
+#define RESTORE_MATH_UTILS_HPP
+
+#include <opencv2/core/core.hpp>
+
+namespace ret {
+
+    namespace math {
+
+        template <typename T>
+        T clamp(T val, T min, T max) {
+            return val < min ? min : (val > max ? max : val);
+        }
+
+        template <typename T>
+        int signum(T val) {
+            return (val > 0.0) - (val < 0.0);
+        }
+
+        template <typename T>
+        double rad2deg(T val) {
+            return val * 180.0 / M_PI;
+        }
+
+        template <typename T>
+        double deg2rad(T val) {
+            return val * M_PI / 180.0;
+        }
+
+        template <typename T>
+        cv::Mat rotMatrix(double angle, T axis, bool rad) {
+            if (!rad) {
+                angle = deg2rad(angle);
+            }
+
+            double s = sin(angle);
+            double c = cos(angle);
+            double mc = 1.0 - c;
+
+            axis /= norm(axis);
+            double x = axis[0];
+            double y = axis[1];
+            double z = axis[2];
+
+            // clang-format off
+            cv::Mat r = (cv::Mat_<double>(3, 3) <<
+                         x*x*mc + c,   x*y*mc - z*s, x*z*mc + y*s,
+                         x*y*mc + z*s, y*y*mc + c,   y*z*mc - x*s,
+                         x*z*mc - y*s, y*z*mc + x*s, z*z*mc + c);
+            // clang-format on
+
+            return r;
+        }
+    }
+}
+
+#endif
---
+++ test/utils_test.cpp
@@ -0,0 +1,76 @@
+/*
+ * Copyright (c) 2015, Kai Wolf
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE. */
+
+#include <gtest/gtest.h>
+#include <restore/math.hpp>
+
+using namespace ret::math;
+
+TEST(UtilsTest, ClampMax) {
+
+    double maxVal = 17.0;
+    ASSERT_DOUBLE_EQ(maxVal, clamp(23.0, 0.0, maxVal));
+}
+
+TEST(UtilsTest, ClampMin) {
+
+    double minVal = 3.0;
+    ASSERT_DOUBLE_EQ(minVal, clamp(1.7, minVal, 10.0));
+}
+
+TEST(UtilsTest, ClampNegativeNumber) {
+
+    double minVal = 0.0;
+    ASSERT_DOUBLE_EQ(minVal, clamp(-3.2, minVal, 10.0));
+}
+
+TEST(UtilsTest, SignPositive) {
+
+    ASSERT_EQ(1, signum(17));
+}
+
+TEST(UtilsTest, SignNeutral) {
+
+    ASSERT_EQ(0, signum(0));
+}
+
+TEST(UtilsTest, SignNegative) {
+
+    ASSERT_EQ(-1, signum(-23));
+}
+
+TEST(UtilsTest, Rad2DegTest) {
+
+    double angle;
+    for (int i = -10; i < 10; ++i) {
+        angle = rad2deg(i * M_PI);
+        ASSERT_DOUBLE_EQ(i * 180, angle);
+    }
+}
+
+TEST(UtilsTest, Deg2RadTest) {
+
+    double angle;
+    for (int i = -10; i < 10; ++i) {
+        angle = deg2rad(i * 180);
+        ASSERT_DOUBLE_EQ(i * M_PI, angle);
+    }
+}
