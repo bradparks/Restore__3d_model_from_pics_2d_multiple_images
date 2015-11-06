646e9a40aa7eb09db8aba944668886c7eca7b015
Kai Wolf <kai.wolf@gmail.com>
2015-11-06 08:59:29 +0100

    Refactor opencv get image type

--- src/common/utils.hpp
+++ src/common/utils.hpp
@@ -27,6 +27,7 @@
 // C++ system files
 #include <cmath>
 #include <cstddef>
+#include <map>
 #include <memory>
 #include <type_traits>
 #include <utility>
@@ -85,29 +86,25 @@ namespace ret {
 
     inline std::string getImgType(int img_type_int) {
         // 7 base types, with five channel options each (none or C1, ..., C4)
-        auto numImgTypes = 35;
-        int enum_ints[] = {CV_8U,  CV_8UC1,  CV_8UC2,  CV_8UC3,  CV_8UC4,
-                           CV_8S,  CV_8SC1,  CV_8SC2,  CV_8SC3,  CV_8SC4,
-                           CV_16U, CV_16UC1, CV_16UC2, CV_16UC3, CV_16UC4,
-                           CV_16S, CV_16SC1, CV_16SC2, CV_16SC3, CV_16SC4,
-                           CV_32S, CV_32SC1, CV_32SC2, CV_32SC3, CV_32SC4,
-                           CV_32F, CV_32FC1, CV_32FC2, CV_32FC3, CV_32FC4,
-                           CV_64F, CV_64FC1, CV_64FC2, CV_64FC3, CV_64FC4};
-        std::string enum_strings[] = {
-            "CV_8U",  "CV_8UC1",  "CV_8UC2",  "CV_8UC3",  "CV_8UC4",
-            "CV_8S",  "CV_8SC1",  "CV_8SC2",  "CV_8SC3",  "CV_8SC4",
-            "CV_16U", "CV_16UC1", "CV_16UC2", "CV_16UC3", "CV_16UC4",
-            "CV_16S", "CV_16SC1", "CV_16SC2", "CV_16SC3", "CV_16SC4",
-            "CV_32S", "CV_32SC1", "CV_32SC2", "CV_32SC3", "CV_32SC4",
-            "CV_32F", "CV_32FC1", "CV_32FC2", "CV_32FC3", "CV_32FC4",
-            "CV_64F", "CV_64FC1", "CV_64FC2", "CV_64FC3", "CV_64FC4"};
+        // clang-format off
+        std::map<int, std::string> cv_types{
+            {CV_8U,    "CV_8U"},    {CV_8UC1,  "CV_8UC1"},  {CV_8UC2,  "CV_8UC2"},
+            {CV_8UC3,  "CV_8UC3"},  {CV_8UC4,  "CV_8UC4"},  {CV_8S,    "CV_8S"},
+            {CV_8SC1,  "CV_8SC1"},  {CV_8SC2,  "CV_8SC2"},  {CV_8SC3,  "CV_8SC3"}, 
+            {CV_8SC4,  "CV_8SC4"},  {CV_16U,   "CV_16U"},   {CV_16UC1, "CV_16UC1"},
+            {CV_16UC2, "CV_16UC2"}, {CV_16UC3, "CV_16UC3"}, {CV_16UC4, "CV_16UC4"},
+            {CV_16S,   "CV_16S"},   {CV_16SC1, "CV_16SC1"}, {CV_16SC2, "CV_16SC2"},
+            {CV_16SC3, "CV_16SC3"}, {CV_16SC4, "CV_16SC4"}, {CV_32S,   "CV_32S"},
+            {CV_32SC1, "CV_32SC1"}, {CV_32SC2, "CV_32SC2"}, {CV_32SC3, "CV_32SC3"},
+            {CV_32SC4, "CV_32SC4"}, {CV_32F,   "CV_32F"},   {CV_32FC1, "CV_32FC1"},
+            {CV_32FC2, "CV_32FC2"}, {CV_32FC3, "CV_32FC3"}, {CV_32FC4, "CV_32FC4"},
+            {CV_64F,   "CV_64F"},   {CV_64FC1, "CV_64FC1"}, {CV_64FC2, "CV_64FC2"},
+            {CV_64FC3, "CV_64FC3"}, {CV_64FC4, "CV_64FC4"},
+        };
+        // clang-format on
 
-        for (auto i = 0; i < numImgTypes; i++)
-            if (img_type_int == enum_ints[i]) {
-                return enum_strings[i];
-            }
-
-        return "unknown image type";
+        auto type = cv_types.find(imgTypeInt);
+        return type == cv_types.end() ? "unknown image type" : type->second;
     }
 }
 
